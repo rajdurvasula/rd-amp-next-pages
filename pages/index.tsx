ccdc247b54f84bf75986bbc24149111490a740f3
Rajasekhar Durvasula <rajasekhar.durvasul@g2w3mynywc.regeneron.regn.com>
2025-07-07 11:26:55 +0530

    added per-user data isolation fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId} todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
